bfcf2d8193a17e71f1203724dd9a694821076503
noah1365 <borlagdatanjohnlloyd0@gmail.com>
2025-03-26 22:30:37 +0800

    Update CORS origin for new render environment URL

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "https://hr3.jjm-manufacturing.com","https://hr3-2htq.onrender.com"],
+    origin: ["http://localhost:5173", "https://hr3.jjm-manufacturing.com","https://hr3-jjm-manufacturing-8lav.onrender.com"],
     credentials: true,
     methods: ["GET", "POST","PUT","DELETE"], 
     allowedHeaders: ["Content-Type", "Authorization"], 
