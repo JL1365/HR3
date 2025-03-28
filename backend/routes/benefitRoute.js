eef137d56581a4f5ce4b50503c7019930f13216c
noah1365 <borlagdatanjohnlloyd0@gmail.com>
2025-03-28 16:52:07 +0800

    Add endpoint to calculate 13th-month pay; aggregate payroll data and return calculated results

--- backend/routes/benefitRoute.js
+++ backend/routes/benefitRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import {getAllEmployeeBenefitDetails, getUploadedDocuments, uploadDocument } from '../controllers/benefitController.js';
+import {getAllEmployeeBenefitDetails, getUploadedDocuments, uploadDocument, calculate13MonthPay } from '../controllers/benefitController.js';
 
 import upload from '../configs/multerConfig.js';
 
@@ -10,5 +10,7 @@ const benefitRoute = express.Router();
 benefitRoute.get("/get-all-employee-benefit-details", getAllEmployeeBenefitDetails);
 benefitRoute.post("/send-benefit-documents",upload.single('documentFile'),uploadDocument);
 benefitRoute.get("/get-uploaded-documents", getUploadedDocuments);
+
+benefitRoute.get("/calculate-13-month", calculate13MonthPay);
 
 export default benefitRoute;
